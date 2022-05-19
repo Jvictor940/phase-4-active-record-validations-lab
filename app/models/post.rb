5e1b085eb3b72e74d0e5d566984bd9e1fd170d63
Jerry <Jvictor940@gmail.com>
2022-05-19 17:42:47 -0400

    commit tree 73b4aed2ba6d47fb1adddf210fb1d80434df05c2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { min: 250}
+    validates :summary, length: { max: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
 end
